25eb06f35cf567217769f9040bf48d0f3e6144cb
Nikita Nikita <jassie.jassie@yandex.ru>
2023-08-15 19:11:21 +0300

    Задание 4.2.11 Цикл do while со счетчиком t

--- Module4App/Program.cs
+++ Module4App/Program.cs
@@ -3,43 +3,84 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-        
-        var color = Console.ReadLine();
-
-        switch(color)
+        Console.WriteLine("Цикл While:");
+        int k = 0;
+        while (k < 3)
         {
-            case "red":
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(k);
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            switch (Console.ReadLine())
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is red!");
 
-                Console.WriteLine("Your color is red!");
+                    break;
 
-                break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is green!");
 
-            case "green":
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
+                    break;
 
-                Console.WriteLine("Your color is green!");
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is cyan!");
 
-                break;
-            
-            case "cyan":
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
+                    break;
 
-                Console.WriteLine("Your color is cyan!");
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is Yellow now!");
 
-                break;
+                    break;
 
-            default: 
-                Console.BackgroundColor = ConsoleColor.Yellow;
-                Console.ForegroundColor = ConsoleColor.Red;
-
-                Console.WriteLine("Your color is cyan!");
-
-                break;
+            }
+            k++;
         }
+        Console.WriteLine("Цикл Do While:");
+        int t = 0;
+        do
+        {
+            Console.WriteLine(t);
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            switch (Console.ReadLine())
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is red!");
+
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is green!");
+
+                    break;
+
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is cyan!");
+
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is Yellow now!");
+
+                    break;
+
+            }
+            t++;
+        }
+        while (t < 3);
     }
 }
