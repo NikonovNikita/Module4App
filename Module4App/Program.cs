daff8fa8028a2a2e039a13c009bf687141e14819
Nikita Nikita <jassie.jassie@yandex.ru>
2023-08-17 20:56:14 +0300

    Задание 4.3.7

--- Module4App/Program.cs
+++ Module4App/Program.cs
@@ -3,45 +3,14 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        string[] favcolors = new string[3];
-        for(int i = 0; i < favcolors.Length; i++)
+        Console.Write("Введите свое имя ");
+        string name = Console.ReadLine();
+        Console.Write("Буквы вашего имени в обратном порядке: ");
+        for (int i = name.Length - 1; i >= 0; i --)
         {
-            Console.WriteLine("Введите любимый цвет номер {0}", i + 1);
-            favcolors[i] = Console.ReadLine();
+            Console.Write(name[i] + " ");
         }
-        foreach (var color in favcolors)
-        {
-        switch (color)
-            {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Your color is red!");
 
-                    break;
-
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Your color is green!");
-
-                    break;
-
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Your color is cyan!");
-
-                    break;
-
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Your color is Yellow now!");
-
-                    break;
-
-            }
-        }
+        
     }
 }
