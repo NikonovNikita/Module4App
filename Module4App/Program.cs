e926541ecdfdc4a2619b76d121a1022882202bfb
Nikita Nikita <jassie.jassie@yandex.ru>
2023-08-22 21:39:16 +0300

    Практикум модуля 4. Задание 4.5.1 Задание 4.5.2 Задание 4.5.3 Задание 4.5.4 Задание 4.5.5 Задание 4.5.6

--- Module4App/Program.cs
+++ Module4App/Program.cs
@@ -3,21 +3,45 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        (string Name, string Type, double Age, int NameCount) Pet;
+        (string Name, string LastName, string Login, int LoginLength, 
+            bool HasPet, string[] favcolors, double Age) User;
 
-        Console.Write("Введите имя питомца ");
-        Pet.Name = Console.ReadLine();
-        Pet.NameCount = Pet.Name.Length;
+        for (int x = 0; x < 3; x++)
+        {
 
-        Console.Write("Введите вид питомца ");
-        Pet.Type = Console.ReadLine();
+            Console.Write("Введите имя ");
+            User.Name = Console.ReadLine();
+            Console.Write("Введите вашу фамилию ");
+            User.LastName = Console.ReadLine();
+            Console.Write("Введите логин ");
+            User.Login = Console.ReadLine();
 
-        Console.Write("Введите возраст питомца ");
-        Pet.Age = double.Parse(Console.ReadLine());
+            User.LoginLength = User.Login.Length;
 
-        Console.WriteLine("Имя вашего питомца {0}", Pet.Name);
-        Console.WriteLine("Вид вашего питомца {0}", Pet.Type);
-        Console.WriteLine("Возраст вашего питомца {0}", Pet.Age);
-        Console.WriteLine("Длина имени вашего питомца {0}", Pet.NameCount);
+            Console.Write("У вас есть животные? ");
+            var result = Console.ReadLine();
+
+            if (result == "Да")
+            {
+                User.HasPet = true;
+            }
+            else
+            {
+                User.HasPet = false;
+            }
+
+            Console.Write("Введите возраст ");
+            User.Age = double.Parse(Console.ReadLine());
+
+            User.favcolors = new string[3];
+            Console.WriteLine("Введите три любимых цвета (цвет + Enter) ");
+
+            for (int i = 0; i < User.favcolors.Length; i++)
+            {
+                User.favcolors[i] = Console.ReadLine();
+            }
+        }
+
     }
 }
+
